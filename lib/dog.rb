2601b38c17b2658340ad62f3aaf99f4d86c14e44
+ <+>
2022-01-13 10:58:20 -0500

    commit tree 5a038d3c5a17d2bc807792b5f02ca5e84e1a6b11

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,67 @@
 class Dog
+    attr_accessor :name, :breed, :id
+    def initialize(name:, breed:, id: nil)
+        @id = id 
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+    def self.drop_table 
+        sql = <<-SQL
+        DROP TABLE IF EXISTS dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+    def save
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+    end
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save 
+    end
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+    def self.all 
+        sql = <<-SQL
+        SELECT * FROM dogs
+        SQL
+        DB[:conn].execute(sql).map do |dog|
+            self.new_from_db(dog)
+        end
+
+    end
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE dogs.name = ?
+        SQL
+        DB[:conn].execute(sql, name).map do |dog|
+            self.new_from_db(dog)
+        end.first
+    end
+    def self.find(id)
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE dogs.id = ? 
+        SQL
+        DB[:conn].execute(sql, id).map do |dog|
+            self.new_from_db(dog)
+        end.first
+    end
 
 end
